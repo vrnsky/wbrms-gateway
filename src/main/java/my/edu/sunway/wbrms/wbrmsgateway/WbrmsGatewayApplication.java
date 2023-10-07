596ce22511337dea1c6c2e73cadb0efb000739ad
vrnsky <vrnsky@protonmail.ch>
2023-10-07 17:41:09 +0800

    Initial commit without any implementation

---
+++ src/main/java/my/edu/sunway/wbrms/wbrmsgateway/WbrmsGatewayApplication.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbrmsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbrmsGatewayApplication.class, args);
+	}
+
+}
